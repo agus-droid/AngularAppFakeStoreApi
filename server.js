f6d0427969fccb055b8717a2f6814329dccdb6cc
AgustinSosaBertolini <agustinsosabertolini@gmail.com>
2022-01-25 20:14:37 -0300

    Arreglo la ruta de nuevo y remuevo dependencias innecesarias

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/AngularAppFakeStoreApi'));
+app.use(express.static('./dist/my-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/AngularAppFakeStoreApi/'}),
+    res.sendFile('index.html', {root: 'dist/my-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
