668aa48de7efe016c735306e52f5b2c7bb783840
Agus <agustinsosabertolini@gmail.com>
2022-01-29 23:39:53 -0300

    Arreglo login

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,18 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/class/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
   public isCollapsed: boolean;
-  constructor() {
+  public user: User = new User();
+
+  constructor(
+    private userService: UserService,
+    private cookies: CookieService,
+    private toastr: ToastrService
+  ) {
     this.isCollapsed = true
-   }
+  }
 
   ngOnInit(): void {
+    if (this.isLogged()) {
+      this.getUserLogged();
+    }
+  }
+
+  isLogged(): boolean {
+    return this.cookies.check('token');
+  }
+
+  logout() {
+    this.userService.logout();
+    this.toastr.success('Logout successful', 'Success');
+
+  }
+
+  getUserLogged() {
+    this.userService.getUserLogged().subscribe({
+      next: (user: any) => {
+        this.user = user;
+      },
+      error: () => {
+        console.log('Error');
+      }
+    });
   }
 
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { Product } from 'src/app/class/product';
 import { ProductService } from 'src/app/services/product.service';
 import { ToastrService } from 'ngx-toastr';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-products',
@@ -19,12 +18,10 @@ export class ProductsComponent implements OnInit {
     private productService: ProductService,
     private router: Router,
     private toastr: ToastrService,
-    private userService: UserService
   ) { }
 
   ngOnInit(): void {
     this.getAll();
-    this.getUserLogged();
   }
 
   getAll() {
@@ -34,7 +31,6 @@ export class ProductsComponent implements OnInit {
       next: (products: Product[]) => {
         this.products = products;
         this.loading = false;
-        this.toastr.success('Hello world!', 'Toastr fun!');
       },
       error: () => {
         this.error = true;
@@ -46,16 +42,5 @@ export class ProductsComponent implements OnInit {
 
   showProduct(product: Product) {
     this.router.navigate(['/product', product.id]);
-  }
-
-  getUserLogged() {
-    this.userService.getUserLogged().subscribe({
-      next: (user: any) => {
-        console.log(user);
-      },
-      error: () => {
-        console.log('Error');
-      }
-    });
   }
 }
